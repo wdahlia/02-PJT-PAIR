09ced36ae68c675975a818b3fb3ab9dfd983fb88
dahlia <wlstnrdahlia@gmail.com>
2022-10-07 17:17:52 +0900

    pjt edit background-image

--- pjt/urls.py
+++ pjt/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("review/", include("review.urls")),
+    path("", include("review.urls")),
 ]
